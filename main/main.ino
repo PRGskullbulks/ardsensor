9f6e98bcb51cf28d122ea598f74d3f6f20d80cbb
William Florez <william@interface.ca>
2018-04-15 15:39:18 -0500

    code for testing temperature threshold

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,10 @@
+/*
+ * LIBRERIA PARA EL DISPLAY DE CRISTAL LIQUIDO DE 2X16
+ * 
+ */
+
+#include <LiquidCrystal.h>
+
 /*VARIABLES INTERNAS
  * 
  * 
@@ -12,7 +19,9 @@ float tempC; // Variable para almacenar el valor obtenido del sensor (0 a 1023)
  * Inicialmente simularemos el umbral de temperatura con LM35, pero esto usará
  * una termocupla, lo conectamos al pin A0
  */
- const int pinLM35 = 0; // Variable del pin de entrada del sensor (A0)
+ const int pinLM35 = 0; // sensor de temperatura
+ const int pot1 = 1;
+ const int pot2 = 2;
  
 /*ELECTROVALVULAS
  * 
@@ -20,9 +29,9 @@ float tempC; // Variable para almacenar el valor obtenido del sensor (0 a 1023)
  * para que estas activen la resistencia
  */
 
-const int pinValEnt = 1; //simulación de la electroválvula de entrada de agua (pin 1)
-const int pinValSal = 2; //simulación de la electroválvula de salida de agua (pin 2)
-
+int pinValEnt = 3; //simulación de la electroválvula de entrada de agua (pin 1)
+int pinValSal = 4; //simulación de la electroválvula de salida de agua (pin 2)
+int valTmpSen = 0;
 /*RESISTENCIA DE SALIDA
  * La resistencia de salida se activará de acuerdo a las siguientes características:
  * 
@@ -30,19 +39,31 @@ const int pinValSal = 2; //simulación de la electroválvula de salida de agua (
  * + la temperatura del agua esté por debajo de los 40°C
  */
 void setup() {
- 
+  pinMode(pinValEnt, INPUT_PULLUP);
+  pinMode(pinValSal, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pinValEnt), setPul1, LOW);
+  attachInterrupt(digitalPinToInterrupt(pinValSal), setPul2, LOW);
+  Serial.begin(9600);
 }
  
 void loop() {
   // Con analogRead leemos el sensor, recuerda que es un valor de 0 a 1023
-  tempC = analogRead(pinLM35); 
+  //tempC = analogRead(pinLM35); 
    
   // Calculamos la temperatura con la fórmula
-  tempC = (5.0 * tempC * 100.0)/1024.0; 
- 
-
-  
-  // Esperamos un tiempo para repetir el loop
-  delay(1000);
-
+  tempC = (5.0 * tempC * 100.0)/1024.0;
+  valTmpSen = analogRead(pinLM35);
+  Serial.println(valTmpSen);
+  delay(500);
 }
+
+void setPul1()
+{
+  delay(1);
+}
+
+void setPul2()
+{
+  delay(1);
+}
+
