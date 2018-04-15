3e1915c01ebe4843774e18cf5f93fed66eff64c7
William Florez <william@interface.ca>
2018-04-15 17:14:48 -0500

    6

--- main/main.ino
+++ main/main.ino
@@ -22,6 +22,7 @@ float tempC; // Variable para almacenar el valor obtenido del sensor (0 a 1023)
  const int pinLM35 = 0; // sensor de temperatura
  const int pot1 = 1;
  const int pot2 = 2;
+ const int ledP = 13;
  
 /*ELECTROVALVULAS
  * 
@@ -32,6 +33,9 @@ float tempC; // Variable para almacenar el valor obtenido del sensor (0 a 1023)
 int pinValEnt = 3; //simulación de la electroválvula de entrada de agua (pin 1)
 int pinValSal = 4; //simulación de la electroválvula de salida de agua (pin 2)
 int valTmpSen = 0;
+int valElcEnt = 0;
+int valElcSal = 0;
+int valTmpMed = 0;
 /*RESISTENCIA DE SALIDA
  * La resistencia de salida se activará de acuerdo a las siguientes características:
  * 
@@ -39,32 +43,45 @@ int valTmpSen = 0;
  * + la temperatura del agua esté por debajo de los 40°C
  */
 void setup() {
-  pinMode(pinValEnt, INPUT_PULLUP);
-  pinMode(pinValSal, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(pinValEnt), setPul1, LOW);
-  attachInterrupt(digitalPinToInterrupt(pinValSal), setPul2, LOW);
+  //pinMode(pinValEnt, INPUT_PULLUP);
+  pinMode(pinValEnt, INPUT);
+  pinMode(pinValSal, INPUT);
+  pinMode(ledP,OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(pinValEnt), setPul1, HIGH);
+  attachInterrupt(digitalPinToInterrupt(pinValSal), setPul2, HIGH);
   Serial.begin(9600);
 }
  
 void loop() {
+  digitalWrite(ledP,LOW);
   // Con analogRead leemos el sensor, recuerda que es un valor de 0 a 1023
   //tempC = analogRead(pinLM35); 
    
   // Calculamos la temperatura con la fórmula
-  tempC = (5.0 * tempC * 100.0)/1024.0;
+  //tempC = (2.0 * tempC * 100.0)/1024.0;
   valTmpSen = analogRead(pinLM35);
-  Serial.print(valTmpSen);
-  Serial.println(" valor voltaje");
-  delay(500);
+  valTmpMed = (0.42 * valTmpSen * 100.0)/1024.0;
+  if(valTmpMed <= 40) {
+   digitalWrite(ledP,HIGH);
+  }
+  //Serial.print(" valor voltaje :");
+  //Serial.print(valTmpSen);
+  //Serial.print("Temperatura: ");
+  //Serial.print(valTmpMed);
+  //Serial.print(" valor pin 1: ");
+  //Serial.print(pinValEnt);
+  //Serial.print(" valor pin 2: ");
+  //Serial.println(".");
+  delay(100);
 }
 
 void setPul1()
 {
-  delay(1);
+  valElcEnt = 1;
 }
 
 void setPul2()
 {
-  delay(1);
+  valElcSal = 1;
 }
 
